34acbdd26a2df2754cc887d5dbde4087155b9c58
naranjohdez <anaher03@gmail.com>
2016-07-02 16:47:27 -0600

    Added init for blog in index

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,41 +1,26 @@
 'use strict';
-
 // WE DEFINED A GLOBAL OBJECT (Im really proud of this :D )
 var SEED = {
-
-  //Start the Ajax request
-  getBlog: function () {
-    jQuery.ajax({
-          url: '/admin/getBlog.php',
-          type: 'GET'
+    //Start the Ajax request
+    getBlog: function () {
+        jQuery.ajax({
+            url: '/admin/getBlog.php',
+            type: 'GET'
         }).done(function (response) {
-          var data = JSON.parse(response)
-          jQuery.each(data, function (index, value) {
-            jQuery('.gridder').append(
-              '<li class="gridder-list" data-griddercontent="' + index + '">' +
-                '<img src="' + value.pathImg + '"/>'                          +
-                '<span class="title">' + value.titulo + '</span>'       +
-              '</li>'
-            );
-          });
+            var data = JSON.parse(response)
+              jQuery.each(data, function (index, value) {
+                  jQuery('.gridder').append(
+                    '<li class="gridder-list" data-griddercontent="' + index + '">' +
+                    '<img src="' + value.pathImg + '"/>'                          +
+                    '<span class="title">' + value.titulo + '</span>'       +
+                    '</li>'
+                );
+              });
+        })
+        .fail(function (response) {
+            console.warn("Something went wrong with this error: ", response.status);
+            console.warn("Something went wrong with this error: ", response.statusText);
         });
-  }
-
-  // arr = $.map(JSON.parse(data), function(el) { return el });
-  //       for (i=0;i<arr.length;i++){
-  //           $('#result').append(
-  //               '<form>'+
-  //               '<input type="text" name="pTitulo" value ="'+arr[i].titulo+'">'+
-  //               '<input type="text"  name="pNombreAutor" value ="'+arr[i].nombreAutor+'">'+
-  //               '<textarea name="pCuerpo" >'+arr[i].cuerpo+'</textarea>'+
-  //               '<input type="date" name="pFecha" value ="'+arr[i].fecha+'">'+
-  //               '<input type="checkbox" checked ='+arr[i].pBit+' name="pBit">Ingles '+
-  //               '<button onclick = "UpdateBlog('+i+')">Guardar</button>'+
-  //               '<button onclick = "DeleteBlog('+i+')">Eliminar</button>'+
-  //               '</form>');
-  //       }
-//<li class="gridder-list" data-griddercontent="#content1">
-                //     <img src="" />
-                // </li>
-
+    }
 };
+SEED.getBlog();
