74c9177d854c1108bb476e79c6ae9a61bd0a16b7
Bruno Sanchez <brunosanchez85@gmail.com>
2016-07-03 21:59:04 -0600

    added demo with gridder

--- public/js/blog.js
+++ public/js/blog.js
@@ -4,23 +4,36 @@ var SEED = {
     //Start the Ajax request
     getBlog: function () {
         jQuery.ajax({
-            url: '/admin/getBlog.php',
-            type: 'GET'
+            url: 'admin/getBlogAdmin.php',
+            type: 'POST',
         }).done(function (response) {
             var data = JSON.parse(response)
               jQuery.each(data, function (index, value) {
                   jQuery('.gridder').append(
                     '<li class="gridder-list" data-griddercontent="' + index + '">' +
-                    '<img src="' + value.pathImg + '"/>'                          +
-                    '<span class="title">' + value.titulo + '</span>'       +
+                    '<img src="' + value.pathImg + '"/>' +
+                    '<span class="title">' + value.titulo + '</span>' +
                     '</li>'
                 );
+                  jQuery('.gridder').after(
+                    '<div id="content' + index +'" class="gridder-content">' + value.cuerpo + '</div>'
+                    );
               });
         })
         .fail(function (response) {
             console.warn("Something went wrong with this error: ", response.status);
             console.warn("Something went wrong with this error: ", response.statusText);
         });
+    },
+
+    gridder: function () {
+        $('.gridder-show').hide();
+        $('.gridder-list').on('click', function() {
+            $('.gridder-show h2').text('Blog ' + this.id);
+            $('.gridder-show').show();
+        });
+
     }
 };
-SEED.getBlog();
+SEED.gridder();
+// SEED.getBlog();
